8493096add7eb4b961ef43533cd06daeb64feae0
DJeovannyZ <darwinzaldana@gmail.com>
2023-06-17 07:02:35 -0600

    declaration functions for any type of Employee

--- model/ModelEmployee.h
+++ model/ModelEmployee.h
@@ -3,13 +3,37 @@
 namespace model{
     class ModelEmployee{
 
-    void sortByLastName();
 
-    std::vector<dtos::Employee> getListEmployees();
+    std::vector<dtos::Employee> getListEmployees(std::string pathCSV);
 
-    int getCountManager();
+    std::vector<dtos::Employee> getListManagers(std::string pathCSV);
 
-    int getCountTecnician(); 
+    std::vector<dtos::Employee> getListAreaManagers(std::string pathCSV);
+    
+    std::vector<dtos::Employee> getListSupervisors(std::string pathCSV);
+
+    std::vector<dtos::Employee> getListTechnicians(std::string pathCSV);
+
+
+    int getCountEmployees(std::vector<dtos::Employee> listEmployees);
+
+    int getCountManagers(std::vector<dtos::Employee> listManagers);
+
+    int getCountAreaManagers(std::vector<dtos::Employee> listAreaManagers);
+
+    int getCountSupervisors(std::vector<dtos::Employee> listSupervisors);
+
+    int getCountTechnicians(std::vector<dtos::Employee> listTechnicians);
+
+    std::vector<dtos::Employee> sortByLastName();
+
+    std::vector<dtos::Employee> sortByNetSalary();
+
+    double calculateRent(dtos::Employee employee);
+
+    double calculateAFP(dtos::Employee employee);
+
+    double calculateISSS(dtos::Employee employee);
 
 };
 }
