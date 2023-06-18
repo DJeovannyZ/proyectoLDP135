cf706538f157ce8a01a1fe20d616a1a769dd13a6
DJeovannyZ <darwinzaldana@gmail.com>
2023-06-17 19:05:10 -0600

    modify Controller

---
+++ controller/ControllerEmployee.h
@@ -0,0 +1,12 @@
+#ifndef CONTROLLEREMPLOYEE_H
+#define CONTROLLEREMPLOYEE_H
+#include <string>
+#include "../model//ModelEmployee.h"
+namespace controller{
+    class Controller{
+    std::string getCountEmployees(){
+       
+    }
+};
+}
+#endif //CONTROLLEREMPLOYEE_H
