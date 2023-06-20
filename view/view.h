0a2333136844211b09aca2ca7f83f4c406b9e959
Reyna <mr21082@ues.edu.sv>
2023-06-19 23:22:21 -0600

    idea sobre el menu

---
+++ view/view.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+namespace view {
+    class view {
+        public:
+
+        void mainMenu();
+        void showMenu();
+        void showSubMenuRol();
+        void showSubMenuOrderBy();
+    };
+}
